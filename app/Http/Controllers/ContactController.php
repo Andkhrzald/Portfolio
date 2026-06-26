422f039f78a82859de46a17d6182cb957e7d4acb
Andkhrzald <Maxcyandkh20@gmail.com>
2026-06-27 06:28:00 +0700

    fix: enable contact form email sending, remove dead Project model and migration

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 class ContactController extends Controller
 {
@@ -14,8 +17,18 @@ class ContactController extends Controller
             'message' => 'required|string|max:5000',
         ]);
 
-        // TODO: Kirim email atau simpan ke database
-        // Mail::to('rizaldipt20@gmail.com')->send(new ContactMail($validated));
+        try {
+            Mail::to('rizaldipt20@gmail.com')->send(new ContactMail($validated));
+
+            Log::info('Contact form submission', [
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Failed to send contact email: ' . $e->getMessage());
+
+            return back()->with('error', 'Sorry, something went wrong. Please try again later.');
+        }
 
         return back()->with('success', 'Thank you, ' . e($validated['name']) . '! Your message has been sent successfully. I will get back to you soon.');
     }
---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public array $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: "New Contact Message from {$this->data['name']}",
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.contact',
+        );
+    }
+}
